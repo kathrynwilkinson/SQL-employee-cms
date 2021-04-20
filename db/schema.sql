9682402f3ce202e0ae45ad815159d39276788ce0
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-19 20:41:43 -0500

    Update table names

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,28 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE department_table (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role_table (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY(department_id) REFERENCES department_table(id)
+  FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee_table (
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY(role_id) REFERENCES role_table(id)
-  -- possibly add manager id foreign key reference
+  FOREIGN KEY(role_id) REFERENCES roles(id)
+  -- add manager id foreign key if needed
 );
