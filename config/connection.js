ae2f5e0ffb99cb613848201e29c22fee02142dfc
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-19 20:51:53 -0500

    Add config code

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,19 @@
-// handles routes
+
+const util = require('util');
+const mysql2 = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: 'Elephants1118!',
+    database: 'employee_db',
+});
+
+// Promise wrapper
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
