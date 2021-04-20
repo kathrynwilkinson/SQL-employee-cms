f1ad32ce79ac53a3f3eede855aa3d1441fae540e
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-19 23:22:26 -0500

    Initialize prompts
    Call init function to begin prompts + prune unnecessary code + pseudo-code

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 // Dependencies
 const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
-const express = require('express');
 //TODO: consider adding classes and models rather than loading database //will effect server speed
-const db = require('./config/connection');
+const connection = require('./config/connection');
 
-
-// Constructors //Sequelize //*Using Schema instead
-// db.Department = new Department();
-// db.Employee = new Employee();
-// db.Role = new Role();
 
 // Prompts to Update, View, Add, and Delete (departments, roles + employees)
 const questions = [{
     type: 'list',
     name: 'action',
-    message: 'What do you want to do?',
+    message: 'What would you like to do?',
     choices: [
-        'Update Employee',
         'View All Employees',
         'View All Roles',
         'View All Departments',
-        'View All Employees by Department',
         'Add New Employee',
         'Add New Role',
         'Add New Department',
@@ -29,14 +21,17 @@ const questions = [{
         'Finished']
 }];
 
-//const init = () => {
-    //promptuser()
-    //.then(answer => {
-        //switch (answer.action){
+const init = () => {
+    inquirer.prompt(questions);
 
-//        }
-//    })
-//}
+    //     .then(answer => {
+    //         switch (answer.action){
+
+    //        }
+    //    })
+};
+
+
 
 //const viewEmployees = () => {
 //    console.table(Employees)
@@ -45,26 +40,4 @@ const questions = [{
 
 //view employees, view by dept, view by manager, add employee, remove employee, update employee, update role,
 
-
-// async function start() {
-//     try {
-//         const answers = await prompt(questions)
-//         console.log(answers);
-//         switch (answers.choice) {
-//             case 'show all employees':
-//                 const results = await db.Employee.getAllEmployees()
-//                 start();
-//                 break;
-//             case 'show all departments':
-//                 const results = await db.Department.getAllDepts()
-//                 console.table(results);
-//                 start();
-//                 break;
-//             case 'show all roles':
-//                 const results = await db.Role.getAllRoles()
-//                 start();
-//         }
-//     }
-// }
-
-// no need to use models.js if you're using my sql
+init();
